6798ec40326449ddb48d8d15484a2d8385c43ed0
Sushanth N <sushanthnandeti96@gmail.com>
2024-02-19 20:58:53 -0500

    MongoDB Config and ENV Variables Config

--- index.js
+++ index.js
@@ -8,10 +8,11 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import { error } from "console";
 
 // CONFIGURATIONS 
 
-const __filename = fileURLToPath(import.meta.URL);
+const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 dotenv.config();
 const app = express();
@@ -27,7 +28,7 @@ app.use("/assets", express.static(path.join(__dirname, 'public/assets')));
 
 // FILE STORAGE CONIG
 
-const storage = multer.diskStorage({
+const storage = multer.diskStorage({    
 
     destination: function (req, file, cb){
         cb(null,"public/assets");
@@ -39,3 +40,16 @@ filename : function (req,res,cb) {
 });
 
 const upload = multer({storage});
+
+// MONGOOSE SETUP 
+
+const PORT = process.env.PORT || 6001;
+mongoose.
+    connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }).then(()=>  {
+            app.listen(PORT, () => console.log(`Server Port : ${PORT}`));
+    }).catch((error)=> {
+        console.log(`${error} did not connect`);
+    })
